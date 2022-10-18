f33fbe3fdef701547e8b8afad28f555ee0225c2e
Crodic <113219638+Crodic@users.noreply.github.com>
2022-10-18 15:57:48 +0700

    upload

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+// //Chuỗi
+// var stringName = "Crodic Love Alice";
+
+// console.log(stringName)
+// console.log("Số lượng ký tự: "+ stringName.length)
+// console.log("Vị trí của từ: "+ stringName.indexOf("Okay",10))
+// console.log("Vị trí của từ: "+ stringName.lastIndexOf("Alice"))
+// console.log("Cắt chuỗi: "+stringName.slice(0))
+// console.log("Thay thế: "+ stringName.replace("Crodic","Michio"))
+// console.log("CHuyển chữ Hoa: "+stringName.toLocaleUpperCase())
+// console.log("Chuyển chữ thường: "+stringName.toLocaleLowerCase())
+// console.log("Tách chuỗi: "+stringName.split(""))
+// console.log("Lấy chuỗi từ vị trí: "+stringName.charAt(7))
